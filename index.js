cde3b309f22f1c1d2c143aa5fa1953cea8b19131
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2025-12-11 11:59:50 +0100

    eod
    header en footer gemaakt en een stuk van de index opening.

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mySubmit").onclick = function() {
-    username = document.getElementById("MyText").value;
-    document.getElementById("myH1").textContent = `hello ${username}`;
-}
